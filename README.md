701b61fc2d0607e540986d08e6b99aeed2ba2c00
Melaine Gérard <pro@melaine-gerard.fr>
2021-07-01 16:00:23 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Gazette des loups
+
+A Hermes Plugin that allow you to have a newspaper on your discord server.
---
+++ src/main/java/fr/melaine_gerard/gazettedesloups/GazetteDesLoups.java
@@ -0,0 +1,17 @@
+package fr.melaine_gerard.gazettedesloups;
+
+import fr.blueberry.studio.hermes.api.plugins.Plugin;
+
+
+public final class GazetteDesLoups extends Plugin {
+
+    @Override
+    public void onEnable() {
+    }
+
+
+    @Override
+    public void onDisable() {
+
+    }
+}
